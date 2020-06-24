5b0b57140f1294c978a0bec6ecc6f50b03e0129e
Admin <Admin@192.168.76.242>
2020-06-24 13:20:02 +0900

    crawl

--- school/src/crawl/News.java
+++ school/src/crawl/News.java
@@ -1,5 +1,4 @@
 package crawl;
-
 import java.io.IOException;
 import java.util.Iterator;
 
@@ -7,7 +6,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 public class News {
    public static void main(String[] args) {
       
